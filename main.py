8c2c4c36e58d8eb81d2ae2926c063d098b698673
Guilherme Delazeri <delazeri1997@gmail.com>
2019-11-13 22:50:13 -0300

    Implementação do backpropagation na função exemplo_back_um

--- main.py
+++ main.py
@@ -39,41 +39,85 @@ def propagacao(exemplo,thetas,network):
     return ativacao_final
 
 
-def calculaJ(exemplos,thetas, regularizacao, network):
+def calculaJ(exemplos, thetas, regularizacao, network, learning_rate):
     J = 0
     cont = 0
+    # # print('network')
+    # # print(network)
+
+    D = []
+    for i in range(len(network)):
+        D.append([])
+
     for exemplo in exemplos:
         cont += 1
-        print("Processando exemplo de treinamento " + str(cont))
-
+        # print("Processando exemplo de treinamento " + str(cont))
         entradas = np.array(exemplo[0])
         saidas = np.array(exemplo[1])
 
-        saidas_preditas = propagacao(exemplo, thetas, network)
+        # 1.1 Propaga x(i) e obtém as saídas f(x(i)) preditas pela rede
+        ativacao, saidas_preditas = propagacao(exemplo, thetas, network)
 
-        print("Saidas Preditas para o Exemplo" + str(cont))
-        print(saidas_preditas)
-        print("Saidas Esperadas para o Exemplo" + str(cont))
-        print(saidas)
+        # 1.2 calcula deltas para os neurônios da camada de saída
+        error = saidas_preditas - saidas
+
+        # cria array para armazenar os deltas
+        deltas = []
+        for i in range(len(network)):
+            deltas.append([])
+        deltas[-1] = error
+        # print('ativacao', len(ativacao))
+
+        # 1.3 Para cada camada k=L-1…2, calcula os deltas para as camadas ocultas
+        for k in reversed(range(1, len(network)-1)):
+            delta = np.multiply(np.multiply(np.transpose(thetas[k]) * deltas[-1], ativacao[k]), (1-ativacao[k]))  #.* ativacao[k] .* (1-ativacao[k])
+            delta_without_bias = []
+
+            # Remove o primeiro elemento de delta(l=k) (i.e., o delta associado ao neurônio de bias da camada k
+            for i in range(len(delta)):
+                delta_without_bias.append(np.delete(delta[i], 0, None))
+            deltas[k] = delta_without_bias
+        
+        # 1.4 Para cada camada k=L-1…1, atualiza os gradientes dos pesos de cada camada com base no exemplo atua
+        for k in reversed(range(0, len(network)-1)):
+            factor = deltas[k+1] * np.transpose(ativacao[k])
+            if len(D[k]) == 0:
+                D[k] = factor
+            else:
+                D[k] += factor
+            # print('D[', k, D)
+
+        # 2. Calcula gradientes finais (regularizados) para os pesos de cada camada
+        for k in reversed(range(0, len(network)-1)):
+            # 2.1 aplica regularização λ apenas a pesos não bias
+            thetas[k].insert(0, 0)
+            thetas[k] = geek.insert(arr, 1, 9) 
+            Pk = np.multiply(regularizacao, thetas[k])
+            # 2.2 combina gradientes com regularização; divide pelo num de exemplos para calcular gradiente médio
+            # print('D[' + str(k) + ']')
+            # print(D[k])
+            # print('P[' + str(k) + ']')
+            # print(Pk)
+            print(thetas[k])
+            D[k] = (1/len(exemplos)) * (D[k] + Pk)
+
+        #4. atualiza pesos de cada camada com base nos gradientes
+        for k in reversed(range(0, len(network)-1)):
+            thetas[k] = thetas[k] - np.multiply(learning_rate, D[k])
 
         vetorJ = (np.negative(saidas)).dot( np.log(saidas_preditas))
         vetorJ -= (np.ones(saidas.size) - saidas).dot(np.log(np.ones(saidas_preditas.size) - saidas_preditas))
-
-        print("J para o Exemplo" + str(cont))
-        print(np.sum(vetorJ))
-
         J += np.sum(vetorJ)
 
-    J = J/ len(exemplos)
+    J = J/len(exemplos)
     S = 0
-    for theta_matrix in thetas:
-        for theta_line in theta_matrix:
-            for i in range(1,len(theta_line)): #Evita thetas de bias
-                S+= math.pow(theta_line[i],2)
+    for theta_vector in thetas[0]:
+        for theta in theta_vector:
+            S+= math.sqrt(theta)
     S = regularizacao/(2*len(entradas))*S
 
-    print("J para o total do dataset")
-    print(str(J + S))
+    # print("J para o total do dataset")
+    # print(str(J + S))
     return J + S
 
 def exemplo_back_um(layers,lamb, theta_matrices):
@@ -100,7 +144,7 @@ def exemplo_back_um(layers,lamb, theta_matrices):
         exemplos.append([entradas[i], saidas[i]])
 
     print(exemplos)
-    Jexemplo = calculaJ(exemplos,thetas,regularizacao,network)
+    Jexemplo = calculaJ(exemplos, thetas, regularizacao, network, learning_rate)
 
 def exemplo_back_two(layers,lamb,theta_matrices):
     # 4 camadas [2 4 3 2]
@@ -114,6 +158,7 @@ def exemplo_back_two(layers,lamb,theta_matrices):
     thetas = np.array([theta1,theta2,theta3])
 
     regularizacao = lamb
+    learning_rate = 0.01
 
     entradas = [[0.32, 0.68], [0.83, 0.02]]
     saidas = [[0.75, 0.98], [0.75, 0.28]]
@@ -123,7 +168,7 @@ def exemplo_back_two(layers,lamb,theta_matrices):
         exemplos.append([entradas[i], saidas[i]])
 
     print(exemplos)
-    Jexemplo = calculaJ(exemplos,thetas,regularizacao,network)
+    Jexemplo = calculaJ(exemplos, thetas, regularizacao, network, learning_rate)
 
 
 if __name__ == '__main__':
